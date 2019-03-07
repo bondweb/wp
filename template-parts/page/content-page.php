7ddd9cb101217534f49b3deffa3e5c4ed2dc4505
bondweb <digitaltrace@gmail.com>
2019-03-07 16:57:13 +0100

    Add files via upload

---
+++ template-parts/page/content-page.php
@@ -0,0 +1,34 @@
+
+<div class="card mb-4">
+
+  <a href="<?php the_permalink(); ?>">
+    <?php
+         if ( has_post_thumbnail() ) {
+         the_post_thumbnail('', ['class' => 'card-img-top', 'title' => 'Feature image']);
+}
+?>
+  		</a>
+     <div class="card-body">
+     <h2 class="card-title"><?php the_title(); ?></h2>
+     
+     
+		<?php the_content(); ?>
+	<!--.entry-content-->
+    
+            </div>
+            <div class="card-footer text-muted">
+              Posted on  <?php the_date();?>  by
+              <a href="<?php the_author_link(); ?>"><?php the_author();?></a>
+              
+            
+			
+			
+          
+
+              
+              
+              
+            </div>
+
+
+</div>
